9cce32305edf2411655c56713f454ffcb8c245a0
webwizard <webwizard2022@gmail.com>
2023-06-21 11:02:42 +0530

    21-06-2023(string functions)

--- string.php
+++ string.php
@@ -1,12 +1,55 @@
 <?php
-$name = "Priyanka";
-// $name = 'Priyanka';
-echo "hello $name"; // run if ""
-echo "<br>";
-echo 'hello $name'; // will not run because ''
-echo "<br>";
-// Solution
+// $name = "Priyanka";
+// // $name = 'Priyanka';
+// echo "hello $name"; // run if ""
+// echo "<br>";
+// echo 'hello $name'; // will not run because ''
+// echo "<br>";
+// // Solution
 
-echo "hello " . $name; // concatenation
+// echo "hello " . $name; // concatenation
+// echo "<br>";
+// echo 'hello ' . $name; // concatenation
+
+$name = "khusboo yadav";
+
+$nameLength = strlen($name);
+echo "Number of characters in " . $name . " is : " . $nameLength;
 echo "<br>";
-echo 'hello ' . $name; // concatenation
+
+$nameWordCount = str_word_count($name);
+echo "Number of words in " . $name . " is : " . $nameWordCount;
+echo "<br>";
+
+$reverseName = strrev($name);
+echo "Reverse of " . $name . " is : " . $reverseName;
+echo "<br>";
+
+$searchString = "Yadav";
+echo 'Position of ' . $searchString . ' in ' . $name . ' is: ' . stripos($name, $searchString);
+echo "<br>";
+
+echo str_replace("world", "Dolly", "Hello World!");
+echo "<br>";
+
+echo strtolower($name);
+echo "<br>";
+
+echo strtoupper($name);
+echo "<br>";
+
+echo ucfirst($name);
+echo "<br>";
+
+echo ucwords($name);
+echo "<br>";
+
+echo trim('    Khusboo Yadav    ');
+echo "<br>";
+
+echo rtrim('Khusboo Yadav', 'Yadav');
+echo "<br>";
+
+
+echo ltrim('Khusboo Yadav', 'Khusboo');
+echo "<br>";
