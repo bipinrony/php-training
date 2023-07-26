0246b7c7e897b41f495bf9d9ea2a6d383a0c3317
webwizard <webwizard2022@gmail.com>
2023-07-26 10:19:03 +0530

    26-07-2023

--- calculator.php
+++ calculator.php
@@ -1,25 +1,66 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-// make functions for all airthmetic operations
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        Number1: <input type="number" name="number1"><br>
+        Number2: <input type="number" name="number2"><br>
+        <input type="submit" name="action" value="+">
+        <input type="submit" name="action" value="-">
+        <input type="submit" name="action" value="X">
+        <input type="submit" name="action" value="/">
+    </form>
+</body>
+
+</html>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+
+    $number1 = (int)$_POST['number1'];
+    $number2 = (int)$_POST['number2'];
+    if (empty($number1) || empty($number2)) {
+        echo "Please enter both numbers";
+        exit;
+    }
+    $action = $_POST['action'];
+
+    if ($action == "+") {
+        sum($number1, $number2);
+    } else if ($action == "-") {
+        sub($number1, $number2);
+    } else if ($action == "X") {
+        multiplication($number1, $number2);
+    } else {
+        division($number1, $number2);
+    }
+}
+
 function sum($number1, $number2)
 {
+    echo "Sum of given numbers is: " . ($number1 + $number2);
 }
+
 function sub($number1, $number2)
 {
+    echo "Subtraction of given numbers is: " . ($number1 - $number2);
 }
 
-function multiply($number1, $number2)
+
+function multiplication($number1, $number2)
 {
+    echo "Multiplication of given numbers is: " . ($number1 * $number2);
 }
-function divide($number1, $number2)
+
+
+function division($number1, $number2)
 {
+    echo "Division of given numbers is: " . ($number1 / $number2);
 }
-
-$number1 = 5;
-$number2 = 4;
-
-
-sum($number1, $number2);
-sub($number1, $number2);
-multiply($number1, $number2);
-divide($number1, $number2);
+?>
